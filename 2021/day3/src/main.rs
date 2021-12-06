aadf0fa8dff1938aca9ffd3ff9c10645994da02d
Sylk0s <sylkos49@gmail.com>
2021-12-06 00:10:11 -0500

    Commit before I go to sleep

--- 2021/day3/src/main.rs
+++ 2021/day3/src/main.rs
@@ -5,14 +5,36 @@ fn main() {
         .expect("Could not read file");
 
     let lines: Vec<&str> = file.split("\n").collect();
-    //let lines2: Vec<&str> = file.split("\n").collect();
 
     println!("{}",part_one(&lines));
-    //println!("{}",part_two(lines2));
+    println!("{}",part_two(&lines));
 }
 
 fn part_one(lines: &Vec<&str>) -> u32 {
-    let mut gamma = 0;
+    let gamma = find_common_bits(lines);
+    gamma*(gamma^0b111111111111)
+}
+
+fn part_two(lines: &Vec<&str>) -> u32 {
+    let bits = find_common_bits();
+    let mut possible_solutions = lines.clone();
+
+    for i in 0..lines[0].len()  {
+        for line in possible_solutions {
+            if line
+        }
+    }
+
+    //110110110111
+    //001001000010
+
+    //110110111100
+    //001001000011
+
+    //0b110110110111*0b001001000010
+}
+
+fn find_common_bits(lines: &Vec<&str>) -> u32 {
     let line_length = lines[0].len();
     let mut totals = vec![0; line_length];
 
@@ -25,15 +47,13 @@ fn part_one(lines: &Vec<&str>) -> u32 {
         }
     }
 
+    let mut common_bits = 0;
+    
     for n in 0..line_length {
         if totals[n] > lines.len()/2 {
-            gamma += 1<<n;
+            common_bits += 1<<n;
         }
     }
 
-    gamma*(gamma^0b111111111111)
+    common_bits
 }
-
-//fn part_two() {
-//    return
-//}
