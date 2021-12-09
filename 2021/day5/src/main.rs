60731acd8f4d816e046a4894ec46fb91401d521d
Jake K <68135816+Sylk0s@users.noreply.github.com>
2021-12-09 16:13:42 -0500

    day5 has a logical error, but compiles

--- 2021/day5/src/main.rs
+++ 2021/day5/src/main.rs
@@ -1,17 +1,35 @@
 use std::fs;
 
 fn main() {
-    let file = fs::read_to_string("input4.txt")
+    let file = fs::read_to_string("input5.txt")
         .expect("Could not read file");
 
     let lines: Vec<&str> = file.split("\n").collect();
 
     println!("{}",part_one(&lines));
-    println!("{}",part_two(&lines));
+    //println!("{}",part_two(&lines));
 }
 
-fn part_one(lines: &Vec<&str>) {
+fn part_one(lines: &Vec<&str>) -> u32 {
 
+    let line_vec = Line::parse_to_flat_line(lines);
+
+    let mut total_count = 0;
+    for i in 0..1000 {
+        for j in 0..1000 {
+            let mut count = 0;
+            for line in &line_vec {
+                if line.point_exists(i,j) {
+                    count += 1;
+                }
+                if count > 1 {
+                    total_count += 1;
+                    break;
+                }
+            }
+        }
+    }
+    total_count
 }
 
 struct Line {
@@ -24,17 +42,17 @@ struct Line {
 impl Line {
     fn parse_to_flat_line(lines: &Vec<&str>) -> Vec<Line> {
 
-        let results = Vec::new();
+        let mut results = Vec::new();
 
         for line in lines {
-            let temp_vec = line.split(' -> ').collect();
-            let coord1 = temp_vec[0].split(',').collect();
-            let coord2 = temp_vec[1].split(',').collect();
+            let temp_vec: Vec<&str> = line.split(" -> ").collect();
+            let coord1: Vec<&str> = temp_vec[0].split(',').collect();
+            let coord2: Vec<&str> = temp_vec[1].split(',').collect();
 
-            let x1 = coord1[0].parse();
-            let y1 = coord1[1].parse();
-            let x2 = coord2[0].parse();
-            let y2 = coord2[1].parse();
+            let x1 = coord1[0].trim().parse::<u32>().expect("Parsing error");
+            let y1 = coord1[1].trim().parse::<u32>().expect("Parsing error");
+            let x2 = coord2[0].trim().parse::<u32>().expect("Parsing error");
+            let y2 = coord2[1].trim().parse::<u32>().expect("Parsing error");
 
             if x1 == x2 || y1 == y2 {
                 results.push(Line {x1:x1,y1:y1,x2:x2,y2:y2});
@@ -42,8 +60,20 @@ impl Line {
         }
         results
     }
+}
 
-    fn intersects(&self, line: &Line) -> bool {
-        
+impl Line { // doesnt wanna work unless i do this grrrr
+    fn point_exists(&self,px:u32,py:u32) -> bool {
+        if self.x1 == self.x2 {
+            return is_between(self.y1,self.y2,py);
+        }
+        if self.y1 == self.y2 {
+            return is_between(self.x1,self.x2,px);
+        }
+        false
     }
+}
+
+fn is_between(n1:u32, n2:u32, p:u32) -> bool {
+    (n1 >= p && p >= n2) || (n2 >= p && p >= n1)
 }
