7b1267bc8a9705ae6a78de2c60299c091bd9e1e7
sylkos <sylkos49@gmail.com>
2022-12-06 22:54:21 -0500

    day4 finished

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -14,8 +14,8 @@ fn pt1(l: &Vec<&str>) -> u32 {
     l.iter().filter(|s| **s != "").map(|x| x.split(",").collect()).fold(0, |acc, y: Vec<&str>| {
         let r1: Vec<i32> = y[0].split("-").map(|p| p.parse::<i32>().unwrap()).collect();
         let r2: Vec<i32> = y[1].split("-").map(|p| p.parse::<i32>().unwrap()).collect();
-        if r1[0] > r2[0] && r1[1] < r2[1] ||
-            r1[0] < r2[0] && r1[1] > r2[1] {
+        if r1[0] >= r2[0] && r1[1] <= r2[1] ||
+            r1[0] <= r2[0] && r1[1] >= r2[1] {
             return acc + 1;
         }
         acc
@@ -23,5 +23,16 @@ fn pt1(l: &Vec<&str>) -> u32 {
 }
 
 fn pt2(l: &Vec<&str>) -> u32 {
-    0
+    l.iter().filter(|s| **s != "").map(|x| x.split(",").collect()).fold(0, |acc, y: Vec<&str>| {
+        let r1: Vec<i32> = y[0].split("-").map(|p| p.parse::<i32>().unwrap()).collect();
+        let r2: Vec<i32> = y[1].split("-").map(|p| p.parse::<i32>().unwrap()).collect();
+        // terrible solution ;*(
+        if r1[0] >= r2[0] && r1[0] <= r2[1] ||
+            r2[0] >= r1[0] && r2[0] <= r1[1] ||
+            r1[1] <= r2[1] && r1[1] >= r2[0] ||
+            r2[1] <= r1[1] && r2[1] >= r1[0] {
+            return acc + 1;
+        }
+        acc
+    })
 }
